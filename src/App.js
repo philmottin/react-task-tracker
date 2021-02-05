9efff4c8685c06502e3829e71ca9cfc108babce3
Phil Mottin <philmottin@gmail.com>
2021-02-05 21:00:39 +0000

    Delete tasks from json-server backend
    Update deleteTask method with fecth()

--- src/App.js
+++ src/App.js
@@ -34,8 +34,12 @@ function App() {
   };
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
     console.log('Delete: ', id);
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    });
+
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
