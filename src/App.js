ba452a50d0199ea2915c18d08b29cede4df870a6
Phil Mottin <philmottin@gmail.com>
2021-02-05 18:00:12 +0000

    Add AddTask component
    Add addTask method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,6 +25,15 @@ function App() {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    console.log(task);
+    const id = Math.floor(Math.random() * 10000 + 1);
+    //console.log(id);
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   // Delete Task
   const deleteTask = (id) => {
     console.log('Delete: ', id);
@@ -43,6 +53,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
