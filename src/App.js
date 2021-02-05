2bea2ae32644dadee8f070bb469e0034f6ab2505
Phil Mottin <philmottin@gmail.com>
2021-02-05 16:07:43 +0000

    Add reminder to Task
    Add onToggle method

--- src/App.js
+++ src/App.js
@@ -30,11 +30,21 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    console.log('Reminder: ', id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className='container'>
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         'No tasks to show'
       )}
